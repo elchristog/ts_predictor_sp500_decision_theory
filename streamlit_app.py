dae331e2019ba1ef852696258d568e72d4ccd5b1
Christian <elchristog@gmail.com>
2023-08-01 10:53:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,22 +3,28 @@ import io
 import sys
 
 # Function that will generate the output to be shown
-def generate_output():
+def generate_output(user_input):
     old_stdout = sys.stdout
     new_stdout = io.StringIO()
     sys.stdout = new_stdout
-    some_python_code()  # Your python code goes here
+    some_python_code(user_input)  # Your python code goes here
     output = new_stdout.getvalue()
     sys.stdout = old_stdout
     return output
 
 # Python code to be executed on button press
-def some_python_code():
-    print("This is a sample print statement.")
+def some_python_code(user_input):
+    print(f"This is the user input: {user_input}")
 
 # Streamlit app interface
 st.title('Streamlit Predict Button')
 
+# Text box for user input
+user_input = st.text_input('Enter a string:')
+
 if st.button('Predict'):
-    output = generate_output()
-    st.text(output)
+    if user_input:  # Execute only if user has provided some input
+        output = generate_output(user_input)
+        st.text(output)
+    else:
+        st.warning('Please input a string.')
